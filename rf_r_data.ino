48530fb88a65348f02931a5e210ae5b302f7d1cf
ch-tseng <ch.tseng@sunplusit.com>
2017-01-11 10:41:03 +0800

    Add files via upload

---
+++ rf_r_data.ino
@@ -0,0 +1,127 @@
+#define CE_PIN 8
+#define CSN_PIN 10
+
+#include <SPI.h>
+#include "RF24.h"
+#include <IRremote.h>
+
+IRsend irsend;
+RF24 radio(CE_PIN, CSN_PIN);
+//bool radioNumber = 1;
+//byte addresses[][6] = {"1Node","2Node"};
+
+
+byte address[11] = "SunplusIT-IR-Demo";
+char payload;
+int vals[8];
+//unsigned long vals;
+
+struct dataStruct{
+  int typeIR;
+  unsigned long dataIR;
+  unsigned int bitsIR;  
+}myData;
+
+// 顯示紅外線協定種類
+String getIRProtocol(int irNum) 
+{  
+  String irType;
+  switch(irNum) {
+   case 0:
+    irType = "Unknown";
+    break;    
+   case 1:
+    irType = "NEC";
+    //irsend.sendNEC((myData.dataIR, HEX), myData.bitsIR);
+    break;
+   case 2:
+    irType = "Sony";
+    //irsend.sendSony((myData.dataIR, HEX), myData.bitsIR);
+    break;
+   case 3:
+    irType = "RC5";
+    //irsend.sendRC5((myData.dataIR, HEX), myData.bitsIR);
+    break;
+   case 4:
+    irType = "RC6";
+    //irsend.sendRC6((myData.dataIR, HEX), myData.bitsIR);
+    break;
+   case 5:
+    irType = "SANYO";
+    //irsend.sendNEC((myData.dataIR, HEX), myData.bitsIR);
+    break;
+   case 6:
+    irType = "MITSUBISHI";
+    //irsend.sendNEC((myData.dataIR, HEX), myData.bitsIR);
+    break;
+   case 7:
+    irType = "DISH";
+    //irsend.sendDISH((myData.dataIR, HEX), myData.bitsIR);
+    break;
+   case 8:
+    irType = "SHARP";
+    //irsend.sendSharpRaw((myData.dataIR, HEX), myData.bitsIR);
+    break;
+   case 9:
+    irType = "PANASONIC";
+    //irsend.sendPanasonic((myData.dataIR, HEX), myData.bitsIR);
+    break;
+   case 10:
+    irType = "JVC";
+    //irsend.sendNEC((myData.dataIR, HEX), myData.bitsIR);
+    break;
+   default:
+    irType = "Nodefine"; 
+    //irsend.sendNEC((myData.dataIR, HEX), myData.bitsIR);
+   break;
+  }  
+
+  return irType;
+
+}
+
+void setup() {
+  Serial.begin(115200);
+  radio.begin(); // Start up the radio
+  /*
+  radio.setPALevel(RF24_PA_LOW);
+  if(radioNumber){
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1,addresses[0]);
+  }else{
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1,addresses[1]);
+  }
+  radio.startListening();
+  */
+  
+  radio.setAutoAck(1); // Ensure autoACK is enabled
+  radio.setRetries(15,15); // Max delay between retries & number of retries
+  radio.openReadingPipe(1, address); // Write to device address name
+  radio.startListening();
+  
+}
+
+void loop(void){
+
+  
+  
+  if( radio.available()){
+    radio.stopListening();
+    //Serial.print("OK");
+    while (radio.available()) {                          // While there is data ready
+      radio.read( &myData, sizeof(myData) );             // Get the payload
+    }
+    //sendIRProtocol();
+    Serial.print("Protocol: "); Serial.print(getIRProtocol(myData.typeIR)); 
+    Serial.print(", irCode: "); Serial.print(myData.dataIR, HEX); 
+    Serial.print(", bits: "); Serial.println(myData.bitsIR);
+    
+    //delay(50);
+    radio.startListening();
+
+    
+  }
+
+  delay(500);
+}
---
+++ rf_t_data.ino
@@ -0,0 +1,144 @@
+#define CE_PIN 8
+#define CSN_PIN 10 //Since we are using 3 pin configuration we will use same pin for both CE and CSN
+
+#include "RF24.h"
+#include <IRremote.h>
+
+byte address[11] = "SunplusIT-IR-Demo";
+//byte char_array[] = "4FB48B7";
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+int RECV_PIN = 3;
+IRrecv irrecv(RECV_PIN);
+decode_results results;         // 解碼結果將放在 decode_results 結構的 result 變數裏
+
+struct dataStruct{
+  int typeIR;
+  unsigned long dataIR;
+  unsigned int bitsIR;  
+}myData;
+
+// 顯示紅外線協定種類
+void showIRProtocol(decode_results *results) 
+{
+  Serial.print("Protocol: ");
+  
+  // 判斷紅外線協定種類
+  switch(results->decode_type) {
+   case NEC:
+    myData.typeIR = 1;
+    Serial.print("NEC");
+    break;
+   case SONY:
+    myData.typeIR = 2;
+    Serial.print("SONY");
+    break;
+   case RC5:
+    myData.typeIR = 3;
+    Serial.print("RC5");
+    break;
+   case RC6:
+    myData.typeIR = 4;
+    Serial.print("RC6");
+    break;
+   case SANYO:
+    myData.typeIR = 5;
+    Serial.print("SANYO");
+    break;
+   case MITSUBISHI:
+    myData.typeIR = 6;
+    Serial.print("MITSUBISHI");
+    break;
+   case DISH:
+    myData.typeIR = 7;
+    Serial.print("DISH");
+    break;
+   case SHARP:
+    myData.typeIR = 8;
+    Serial.print("SHARP");
+    break;
+   case PANASONIC:
+    myData.typeIR = 9;
+    Serial.print("PANASONIC");
+    break;
+   case JVC:
+    myData.typeIR = 10;
+    Serial.print("JVC");
+    break;
+   case LG:
+    myData.typeIR = 11;
+    Serial.print("LG");
+    break;
+   case AIWA_RC_T501:
+    myData.typeIR = 12;
+    Serial.print("AIWA_RC_T501");
+    break; 
+   default:
+    myData.typeIR = 0;
+    Serial.print("Unknown encoding");  
+  }  
+
+  // 把紅外線編碼印到 Serial port
+  Serial.print(", irCode: ");            
+  //myData.dataIR = (results->value, HEX);
+  Serial.print(results->value, HEX);    // 紅外線編碼
+  Serial.print(",  bits: ");         
+  myData.bitsIR = results->bits;  
+  Serial.println(myData.bitsIR);        // 紅外線編碼位元數    
+}
+
+void setup() {
+  Serial.begin(115200);
+  irrecv.enableIRIn(); // Start the receiver
+  radio.begin(); // Start up the radio
+  radio.setAutoAck(1); // Ensure autoACK is enabled
+  radio.setRetries(15,15); // Max delay between retries & number of retries
+  radio.openWritingPipe(address); // Write to device address name
+}
+
+void loop(void){
+
+  
+  if (irrecv.decode(&results)) {
+    myData.dataIR = results.value;
+    
+    //Serial.println("");
+    //Serial.print("IR received in HEX ---> ");
+    //Serial.println(myData.dataIR, HEX);    
+    //Serial.print("IR received in INT ---> ");
+    //Serial.println(myData.dataIR);
+    showIRProtocol(&results);
+    //Serial.println("");
+    
+    irrecv.resume(); // Receive the next value
+
+    boolean ok = radio.write( &myData, sizeof(myData) );
+      
+  }
+  /*
+  myData.dataIR = 3214333;
+  myData.bitsIR =32;
+  myData.typeIR = 12;
+  boolean ok = radio.write( &myData, sizeof(myData) );
+  Serial.println("Send...");
+  */
+  /*
+  if (irrecv.decode(&results)) {
+    String stringIR = String(results.value, HEX);
+    char char_array[stringIR.length()+1];
+    stringIR.toCharArray(char_array, stringIR.length()+1);
+    
+    Serial.println("");
+    Serial.print("IR received HEX ---> ");
+    Serial.println(results.value, HEX);
+    Serial.println("");
+    Serial.print("IR received HEX in STRING ---> ");
+    Serial.println(char_array);
+    irrecv.resume(); // Receive the next value
+
+    bool okSend = radio.write( &char_array, strlen(char_array) ); //Send data to 'Receiver' ever second
+  }
+  */
+  delay(2000);
+}
